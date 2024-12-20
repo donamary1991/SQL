765c862b695103bfce9c904ecbf2388fe8aa766c
Dona Mary George <donamarangattu@gmail.com>
2024-12-20 21:50:11 +0530

    Add files via upload

---
+++ customer5.sql
@@ -0,0 +1,12 @@
+-- customer5.csv(if text file .convert txt file to csv file using pandas)
+use june_ds1;
+-- 1. each prof count [count desc]
+select prof,count(*) as abc from customer5 group by prof order by abc desc;
+-- each prof total salary [salary desc] 
+select prof,sum(salary) as abc from customer5 group by prof order by abc desc;
+--  each prof max salary [salary desc]
+select prof,max(salary) as abc from customer5 group by prof order by abc desc;
+-- each loc min salary
+select loc,min(salary) as abc from customer5 group by loc;
+-- each prof average age 
+select prof,avg(age) as abc from customer5 group by prof order by abc desc;
